480e78ce07c664a0e6ed7653e3f3f8d7d3616100
seojiho <happyluck0426@gmail.com>
2025-06-05 10:55:06 +0900

    fix: Firebase 로그인 상태 감지 로직을 useEffect로 이동하여 전역 유저 상태 연동 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect } from "react"
+import { auth } from "./libs/firebase"
 import AppRouter from "./routes/AppRouter"
+import { useUserStore } from "./stores/useUserStore"
 import { GlobalStyle } from "./styles/GlobalStyle"
 
 function App() {
+  const setUser = useUserStore((state) => state.setUser)
+  const clearUser = useUserStore((state) => state.clearUser)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email ?? "",
+          displayName: user.displayName ?? "",
+          photoURL: user.photoURL ?? "",
+        })
+      } else {
+        clearUser()
+      }
+    })
+
+    return () => unsubscribe()
+  }, [setUser, clearUser])
+
   return (
     <>
       <GlobalStyle />
